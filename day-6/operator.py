dcfc2739d0fd347ff31eda2bbccd97dc4cc3de2c
blackrose1302 <58839125+blackrose1302@users.noreply.github.com>
2024-05-13 11:18:25 +0530

    Create operator.py

---
+++ day-6/operator.py
@@ -0,0 +1,57 @@
+#task-1
+import sys
+'''
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+
+print ("Sum is ", a+b)
+print ("Difference is ", a-b)
+print ("Product is ", a*b)
+print ("Quotient is ", a/b)
+
+#task 2
+
+if a>b:
+    print( a ,"is greater than", b)
+if a < b:
+    print (a, "is smaller than", b)
+if a<=b:
+    print (a, "is smaller than equals to", b)
+if a>=b:
+    print (a, "is greater than equals to", b)
+if a==b:
+    print (a, "is equals to", b)
+if a!=b:
+    print (a, "is not equals to", b)
+
+#task 3
+
+x = True
+y = False
+
+print ("AND result are", x and y)
+print ("OR results are", x or y)
+#print ("NOT results are", x not y)
+
+
+
+#task 4
+
+total = 10
+
+total += 2
+total -= 2
+total *= 2
+total /= 2
+
+print (total)
+
+
+#task 6
+
+my_list = [1,2,3,4,5]
+
+
+a=3
+if (a is  my_list):
+    print( a , "is in my list")
